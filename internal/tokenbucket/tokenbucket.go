d8857a1e4a63be06906b7cacaca72a47706e0a15
Sergey Olisov <sergey.olisov@ericsson.com>
2020-02-12 18:18:12 +0300

    Added tokenbucket implementation with tests

---
+++ internal/tokenbucket/tokenbucket.go
@@ -0,0 +1,98 @@
+package tokenbucket
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+// Algorithm description
+// A token is added to the bucket every 1/r seconds
+// The bucket can hold at the most Capacity tokens. If tokens arrives when the bucket is full, it is discarded.
+// when request arrives if at least one token is in the bucket, 1 token removed from the bucket, and the request is allowed.
+// if 0 tokens in the bucket, no tokens removed and the request is considered as not allowed.
+
+// TokenBucket contains elements to implement the algorithm
+type TokenBucket struct {
+	ctx           context.Context
+	capacity      uint32
+	currentAmount uint32
+	rate          time.Duration
+	ticker        *time.Ticker
+	mx            sync.Mutex
+}
+
+var (
+	// ErrTokenBucketInvalidFillRate appears when the rate ==0 is used. To avoid the panic of NewTimeTicker()
+	ErrTokenBucketInvalidFillRate = errors.New("invalid rate, zero value is not allowed")
+)
+
+// NewTokenBucket creates a new instance of TokenBucket
+func NewTokenBucket(ctx context.Context, capacity uint32, rate time.Duration) (*TokenBucket, error) {
+	// To protect for inaccurate function usage. In case of rate == 0  NewTicker will be created with a very loooong period of tick ~ 290 years.
+	// In other terms ticker will not run in closest time.
+	if rate == 0 {
+		return nil, ErrTokenBucketInvalidFillRate
+	}
+
+	tb := &TokenBucket{
+		ctx:           ctx,
+		capacity:      capacity,
+		currentAmount: capacity,
+		rate:          rate,
+		mx:            sync.Mutex{},
+	}
+	// Create a go routine with the time.Ticker to fill the bucket with desired rate.
+	go func(tb *TokenBucket) {
+		tb.ticker = time.NewTicker(tb.rate)
+
+		defer tb.ticker.Stop()
+
+		for {
+			select {
+			case <-tb.ctx.Done():
+				tb.ticker.Stop() // Stops the Ticker
+				return
+			case <-tb.ticker.C:
+				if tb.currentAmount == tb.capacity { //Token Bucken is full all next tokens will be discarded
+					continue
+				}
+				atomic.AddUint32(&tb.currentAmount, 1) // Add one token to the bucket
+			default: // Added to avoid blocking when we have looong time period for ticker
+				continue
+			}
+		}
+	}(tb)
+	return tb, nil
+}
+
+// Allow returns true in case we have tokens in the bucket. One authorization event has a weight of one token
+// When it is allowed to pass, we decrese the CurrentAmount of tokens by 1
+func (tb *TokenBucket) Allow() bool {
+	if tb.currentAmount > 0 { // Bucket is not empty
+		atomic.AddUint32(&tb.currentAmount, ^uint32(0)) // decrease the number of tokens in the bucket
+		return true
+	}
+	return false
+}
+
+// Reset the bucket to the initial state
+func (tb *TokenBucket) Reset() {
+	tb.mx.Lock()
+	tb.currentAmount = tb.capacity
+	tb.mx.Unlock()
+}
+
+// Capacity returns the capacity of the bucket
+func (tb *TokenBucket) Capacity() uint32 {
+	return tb.capacity // It's not necessary to use Mutex as it is a readonly parameter
+}
+
+// Amount returns the current amount of tokens in the bucket
+func (tb *TokenBucket) Amount() uint32 {
+	tb.mx.Lock()
+	defer tb.mx.Unlock()
+	return tb.currentAmount
+}
---
+++ internal/tokenbucket/tokenbucket_test.go
@@ -0,0 +1,176 @@
+package tokenbucket
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+// Testcases:
+// 1. Create a bucket and check that amount of tokens does not decreased when there are no requests to the bucket.
+// 2. Create a bucket and check that it's possible to get all capacity without any cancelation from the bucket in the burst
+// 3. Create an empty bucket and check that all followup requests will be disallowed.
+// 4. Create a bucket which allows to have not more then 10 request per minute
+// 5. Create a bucket with significant capacity, start taking tokens from it, reset bucket. Ensure that bucket returns to initial state: Capacity == currentAmount
+
+// 1. Create a bucket and check that amount of tokens does not decreased when there are no requests to the bucket.
+func TestStillBucket(t *testing.T) {
+
+	capacity := 5
+
+	fillRate := 1 * time.Second
+
+	ctx, done := context.WithCancel(context.Background())
+
+	defer done()
+
+	tb, err := NewTokenBucket(ctx, uint32(capacity), fillRate)
+
+	if err != nil {
+		t.Error("Error", err)
+	}
+
+	time.Sleep(fillRate * 3) // Whait for some time
+
+	if tb.Capacity() != tb.Amount() {
+
+		t.Errorf("Bucket without requests must keep the currentAmoutn=%d equals to defined capacity=%d", tb.Amount(), tb.Capacity())
+
+	}
+
+}
+
+// 2. Create a bucket and check that it's possible to get all capacity without any cancelation from the bucket in the burst
+func TestFullBucket(t *testing.T) {
+
+	var allow bool
+
+	capacity := 5
+	fillRate := 1 * time.Second // Set a quite long period of refill
+
+	ctx, done := context.WithCancel(context.Background())
+
+	tb, err := NewTokenBucket(ctx, uint32(capacity), fillRate)
+
+	if err != nil {
+		t.Error("Error", err)
+	}
+
+	// Gets tokens one by one in number of capacity
+	for i := 0; i < capacity-1; i++ {
+		allow = tb.Allow()
+		if !allow {
+			t.Error("Full TokenBucket must allow all requests regading its capacity")
+		}
+	}
+	done()
+}
+
+// 3. Create an empty bucket and check that all following requests will be disallowed.
+func TestEmptyBucket(t *testing.T) {
+
+	capacity := 3
+
+	fillRate := 5 * time.Second
+
+	ctx, done := context.WithCancel(context.Background())
+
+	defer done()
+
+	tb, err := NewTokenBucket(ctx, uint32(capacity), fillRate)
+
+	if err != nil {
+		t.Error("Error", err)
+	}
+
+	// Make sure that bucket is empty
+	for i := 0; i < capacity-1; i++ {
+		tb.Allow()
+	}
+
+	// While bucket is empty the response must be false
+	for tb.Amount() == 0 {
+		if tb.Allow() {
+			t.Error("Bucket must not allow while it empty")
+			break
+		}
+
+	}
+}
+
+// 5. Create a bucket with significant capacity, start taking tokens from it, reset bucket. Ensure that bucket returns to initial state: Capacity == currentAmount
+func TestResetBucket(t *testing.T) {
+
+	capacity := 100
+
+	fillRate := 1 * time.Second
+
+	ctx, done := context.WithCancel(context.Background())
+
+	defer done()
+
+	tb, err := NewTokenBucket(ctx, uint32(capacity), fillRate)
+
+	if err != nil {
+		t.Error("Error", err)
+	}
+
+	for i := 0; i < 50; i++ {
+		tb.Allow()
+	}
+	if tb.Amount() != tb.Capacity() {
+		tb.Reset()
+		if tb.Amount() != tb.Capacity() {
+			t.Errorf("The bucket must has the capacity=%d and amount=%d are equals after the reset!", tb.Capacity(), tb.Amount())
+		}
+	}
+
+}
+
+// Test Bucket with Zero Capacity, bucket allways must response Allow = false, and keep consistency capacity & currentAmout == 0
+func TestZeroCapacityBucket(t *testing.T) {
+
+	capacity := 0
+
+	fillRate := 1 * time.Millisecond
+
+	ctx, done := context.WithCancel(context.Background())
+
+	defer done()
+
+	tb, err := NewTokenBucket(ctx, uint32(capacity), fillRate)
+
+	if err != nil {
+		t.Error("Error", err)
+	}
+
+	for i := 0; i < 50; i++ {
+		if tb.Allow() {
+			t.Error("Zero capacity bucket must allways response with false")
+		}
+	}
+	// Check the consistency of Bucket
+
+	if !(tb.Capacity() == tb.Amount()) && tb.Amount() != 0 {
+		t.Errorf("Bucket is inconsistent capacity=%d, currentAmount=%d", tb.Capacity(), tb.Amount())
+	}
+
+}
+
+// Test Bucket with Zero fillRate, bucket must response Allow = true for all request belongs his capacity and after that respond Allow = false for forever
+func TestZeroFillRateBucket(t *testing.T) {
+
+	capacity := 10
+
+	fillRate := 0 * time.Millisecond
+
+	ctx, done := context.WithCancel(context.Background())
+
+	defer done()
+
+	_, err := NewTokenBucket(ctx, uint32(capacity), fillRate)
+
+	if err == nil {
+		t.Error("Error cannot create a TimeTicker with rate ==0")
+	}
+}
