59010da613c62dd2efcb43c4d58bb3f498d0d144
Sergey Olisov <sergeyolisov@Black-Mesa.local>
2020-02-16 21:48:16 +0300

    Fixed error description

--- internal/domain/errors/errors.go
+++ internal/domain/errors/errors.go
@@ -5,6 +5,7 @@ import (
 )
 
 var (
-	// ErrTokenBucketInvalidFillRate appears when the rate ==0 is used. To avoid the panic of NewTimeTicker()
+	// ErrTokenBucketInvalidFillRate appears when the parameter rate==0 is used.
+	// To avoid the panic of NewTimeTicker()
 	ErrTokenBucketInvalidFillRate = errors.New("invalid rate, zero value is not allowed")
 )
