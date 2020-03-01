c14b014acfc33726c3e4460a6137a54563d93afc
Sergey Olisov <sergeyolisov@Black-Mesa.local>
2020-03-01 21:20:59 +0300

    [abf-ctl] added client part (does not work yet :(  )

--- cmd/client/abf-ctl.go
+++ cmd/client/abf-ctl.go
@@ -22,13 +22,24 @@ var (
 	client api.ABFServiceClient //nolint
 
 	clientcfg *config.ClientConfig // nolint
+
+	logcfg *config.LoggerConfig // nolint
+
+	login   string //nolint
+	network string // nolint
+
+	color bool // nolint
+
+	ipaddress string //nolint
+
+	ctx context.Context // nolint
 )
 
 // RootCmd is a main command to handle the client commands
 var RootCmd = &cobra.Command{ // nolint
-	Use:       "abf-ctl [add-to-iplist,delete-from-iplist,reset-limits,show-iplist,test",
-	Short:     "abf-ctl gRPC client for AntiBruteForce Service",
-	ValidArgs: []string{"add-to-iplist", "delete-from-iplist", "reset-limits", "show-iplist", "test"},
+	Use:       "abf-ctl",
+	Short:     "abf-ctl gRPC client for ABF Service",
+	ValidArgs: []string{"add", "del", "reset", "show", "test"},
 	Args:      cobra.ExactValidArgs(numberOfValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
 		clientcfg = config.GetClientCfg()
@@ -43,24 +54,59 @@ var RootCmd = &cobra.Command{ // nolint
 			log.Println("Received system interrupt...")
 			cancel()
 		}()
+	},
+}
 
-		switch args[0] {
-		case "add-to-iplist":
-			addToIPList(ctx)
-		case "delete-from-iplist":
-			delFromIPList(ctx)
-		case "reset-limits":
-			resetLimits(ctx)
-		case "show-iplist":
-			showIPList(ctx)
-		case "test":
-			isConform(ctx)
+var addCmd = &cobra.Command{ //nolint
+	Use:   "add",
+	Short: "add to black/white list",
+	Run: func(cmd *cobra.Command, args []string) {
+		r, err := client.AddToIpFilter(ctx, &api.IPFilterData{Network: network, Color: color})
+		if err != nil {
+			log.Fatalf("unable to add to list: %v", err)
 		}
+		log.Println("Done: ", r.Error)
+	},
+}
+
+var delCmd = &cobra.Command{ //nolint
+	Use:   "del",
+	Short: "del from black/white list",
+	Run: func(cmd *cobra.Command, args []string) {
+		r, err := client.DeleteFromIpFilter(ctx, &api.IPFilterData{Network: network, Color: color})
+		if err != nil {
+			log.Fatalf("unable to add to list: %v", err)
+		}
+		log.Println("Done: ", r.Error)
+	},
+}
+var resetCmd = &cobra.Command{ //nolint
+	Use:   "reset",
+	Short: "reset",
+	Long:  "reset the bucket limits for specifi pair of Login/IP",
+	Run: func(cmd *cobra.Command, args []string) {
+		r, err := client.Reset(ctx, &api.AuthRequest{Login: login, Password: "", Ipaddr: ipaddress})
+		if err != nil {
+			log.Fatalf("unable to reset limits: %v", err)
+		}
+		log.Println("Done: ", r.Response)
+	},
+}
+var showCmd = &cobra.Command{ //nolint
+	Use:   "show",
+	Short: "show",
+	Long:  "show dumps the corresponding ip table black/white",
+	Run: func(cmd *cobra.Command, args []string) {
+		r, err := client.GetIpFilters(ctx, &api.IPFilterData{Network: "", Color: color})
+		if err != nil {
+			log.Fatalf("unable to reset limits: %v", err)
+		}
+		log.Println("Done: ", r.Filters)
 	},
 }
 
 func newClient(ctx context.Context, host, port string) api.ABFServiceClient {
-	conn, err := grpc.DialContext(ctx, net.JoinHostPort(host, port))
+	conn, err := grpc.DialContext(ctx, net.JoinHostPort(host, port), grpc.WithInsecure())
 	if err != nil {
 		log.Fatal("Cannot connect to ABF server", err)
 	}
@@ -68,30 +114,24 @@ func newClient(ctx context.Context, host, port string) api.ABFServiceClient {
 	return api.NewABFServiceClient(conn)
 }
 
+func init() { // nolint
+	addCmd.PersistentFlags().StringVarP(&network, "network", "n", "", "network")
+	addCmd.PersistentFlags().BoolVarP(&color, "color", "c", true, "white - true, false black")
+	RootCmd.AddCommand(addCmd)
+	delCmd.PersistentFlags().StringVarP(&network, "network", "n", "", "network")
+	delCmd.PersistentFlags().BoolVarP(&color, "color", "c", true, "white - true, false black")
+	RootCmd.AddCommand(delCmd)
+	resetCmd.PersistentFlags().StringVarP(&login, "login", "l", "", "login to reset the limits")
+	resetCmd.PersistentFlags().StringVarP(&ipaddress, "ipaddress", "i", "", "ip to reset to reset the limits")
+	RootCmd.AddCommand(resetCmd)
+	showCmd.PersistentFlags().BoolVarP(&color, "color", "c", true, "white - true, false black")
+	RootCmd.AddCommand(showCmd)
+}
+
 func main() {
-	log.Println("ABF Client...")
+	log.Println("ABF Client started..")
 
 	if err := RootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func isConform(ctx context.Context) {
-
-}
-
-func addToIPList(ctx context.Context) {
-
-}
-
-func delFromIPList(ctx context.Context) {
-
-}
-
-func resetLimits(ctx context.Context) {
-
-}
-
-func showIPList(ctx context.Context) {
-
 }
--- go.mod
+++ go.mod
@@ -9,10 +9,11 @@ require (
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/lib/pq v1.0.0
 	github.com/prometheus/client_golang v1.4.1
+	github.com/reddaemon/antibruteforce v0.0.0-20200211203859-4edae47b09e0 // indirect
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/cobra v0.0.6
 	github.com/spf13/viper v1.6.2
 	github.com/stretchr/testify v1.4.0
-	go.uber.org/zap v1.10.0
+	go.uber.org/zap v1.13.0
 	google.golang.org/grpc v1.27.1
 )
